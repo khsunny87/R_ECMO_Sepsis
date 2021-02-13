a5e10bf39a584d136a72ae208b225383bac581b4
Sun Kim <yoursunny@gmail.com>
2021-02-13 10:33:57 +0900

    biomarker 분석 추가 in RMD BSI_BM 중간 파일 추가

--- R_script/08_BSI_and_biomarker.R
+++ R_script/08_BSI_and_biomarker.R
@@ -1,31 +1,45 @@
-
+library(moonBook)
+library(dplyr)
+library(readr)
 library(stringr)
-lab_data<-read_csv('Input/lab_merge.txt')
+library(lubridate)
+library(tidyr)
+library(forcats)
+library(sjlabelled)
+
 biomarker_code<-c('WBC Count, Blood'='BL2011', 
                   'Segmented neutrophil'='BL201806',               
                   'CRP, Quantitative (High Sensitivity)'='BL3140',
                   'Procalcitonin, quantitative' ='BL5044')
 
+#lab_data<-read_csv('Input/lab_merge.txt')
 
 
-BSI2<-r_Cx2%>%
-  filter(Cx_result,Cate_sp=='BSI')%>%
-  left_join(.,anal_data%>%select(Basic_Hospital_ID,Insertion_ECMO_시술일,Outcome_ECMO_제거일),by=c('ID'='Basic_Hospital_ID'))%>%
-  filter(lab_performed_time<=Outcome_ECMO_제거일+ddays(1))%>%
-  mutate(interval=(lab_performed_time-Insertion_ECMO_시술일)/ddays(1))%>%
-  filter(interval>=0)%>%
-  group_by(ID,Insertion_ECMO_시술일,Outcome_ECMO_제거일)%>%
-  summarise(interval=min(interval),lab_performed_time=min(lab_performed_time))
+#BSI2<-r_Cx2%>%
+#  filter(Cx_result,Cate_sp=='BSI')%>%
+#  left_join(.,anal_data%>%select(Basic_Hospital_ID,Insertion_ECMO_시술일,Outcome_ECMO_제거일),by=c('ID'='Basic_Hospital_ID'))%>%
+#  filter(lab_performed_time<=Outcome_ECMO_제거일+ddays(1))%>%
+#  mutate(interval=(lab_performed_time-Insertion_ECMO_시술일)/ddays(1))%>%
+#  filter(interval>=0)%>%
+#  group_by(ID,Insertion_ECMO_시술일,Outcome_ECMO_제거일)%>%
+#  summarise(interval=min(interval),lab_performed_time=min(lab_performed_time))
 
 
-BSI_lab<-lab_data%>%
-  filter(검사코드 %in% biomarker_code,ID %in% BSI2$ID)%>%
-  mutate(values=as.numeric(sub('^[^0-9]','',values)))%>%
-  left_join(.,BSI2%>%ungroup()%>%select(ID,lab_performed_time,interval))%>%
-  filter(검사시간<=lab_performed_time+ddays(1))%>%
-  group_by(ID,검사코드)%>%
-  filter(검사시간==max(검사시간))%>%select(-검사명,-단위,-검사시간)%>%
-  pivot_wider(names_from = 검사코드,values_from=values)%>%
+#BSI_lab<-
+#BSI_BM<-lab_data%>%
+#  filter(검사코드 %in% biomarker_code,ID %in% BSI2$ID)%>%
+#  mutate(values=as.numeric(sub('^[^0-9]','',values)))%>%
+#  left_join(.,BSI2%>%ungroup()%>%select(ID,lab_performed_time,interval))%>%
+#  filter(검사시간<=lab_performed_time+ddays(1))%>%
+#  group_by(ID,검사코드)%>%
+#  filter(검사시간==max(검사시간))%>%select(-검사명,-단위,-검사시간)%>%
+#  pivot_wider(names_from = 검사코드,values_from=values)
+
+#write_csv(BSI_BM,'Input/BSI_BM.txt')
+
+BSI_BM<-read_csv('Input/BSI_BM.txt',col_types=cols(ID=col_character()))
+
+BSI_lab<-BSI_BM%>%
   right_join(.,anal_data%>%
                filter(Blood_Cx)%>%select(Basic_Hospital_ID,Outcome_Weaning_success,Outcome_Death),by=c('ID'='Basic_Hospital_ID'))%>%
   ungroup()%>%
@@ -47,9 +61,9 @@ LabName<-function(code){
 BSI_lab<-set_label(BSI_lab,sapply(names(BSI_lab),LabName))
 
 #BSI culture시 biomarker
-BSI_lab%>%
+table6<-BSI_lab%>%
   mytable(Outcome_Weaning_success~.,data=.)
-BSI_lab%>%
+table7<-BSI_lab%>%
   mytable(Outcome_Death~.,data=.)
 
 
